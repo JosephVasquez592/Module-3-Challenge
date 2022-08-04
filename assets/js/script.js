43e2e6f857099e2f09d90c8802afc446028b7c26
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-03 23:59:28 -0600

    created more variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // find place for these.
 var specialCharacters = " !#$%&'()*+,-./:;<=>?@\^_`{|}~".split("");
-console.log(specialCharacters)
+var letters = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = "1234567890".split("");
 // 
-
+console.log(specialCharacters);
+console.log(letters);
+console.log(numbers);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -21,8 +24,12 @@ generateBtn.addEventListener("click", writePassword);
 document.querySelector(".btn").addEventListener('click', promptMe);
 
 function promptMe() {
-    var buttonPrompt = prompt("Use special characters? Y/N");
+    var buttonPrompt = prompt("Use special characters? y/n");
     if (input === "y") {
       
+    } else if () {
+      else if () {
+        
+      }
     }
 }
