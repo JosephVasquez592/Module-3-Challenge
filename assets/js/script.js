a170e1497ca1a54ed6f53d5023be462ad7165b1b
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-03 23:47:45 -0600

    created prompt for button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// find place for these.
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@\^_`{|}~".split("");
+console.log(specialCharacters)
+// 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +17,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+document.querySelector(".btn").addEventListener('click', promptMe);
+
+function promptMe() {
+    var buttonPrompt = prompt("Use special characters? Y/N");
+    if (input === "y") {
+      
+    }
+}
