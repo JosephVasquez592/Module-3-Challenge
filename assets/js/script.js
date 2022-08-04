cd99ac931b1081f691878e040217d50dea03a89e
Joey <107812238+JosephVasquez592@users.noreply.github.com>
2022-08-04 00:15:48 -0600

    created more variables and finishing for the night

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,16 @@ var generateBtn = document.querySelector("#generate");
 var specialCharacters = " !#$%&'()*+,-./:;<=>?@\^_`{|}~".split("");
 var letters = "abcdefghijklmnopqrstuvwxyz".split("");
 var numbers = "1234567890".split("");
-var selections = []
+var selections = [];
+var promptOne = "Use special characters? y/n";
+var promptTwo = "Use letters? y/n";
+var promptThree = "Use numbers? y/n";
 
-// just to check if split works
+// just to check if things work
 console.log(specialCharacters);
 console.log(letters);
 console.log(numbers);
-console.log(selections);
+// console.log(selections);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -24,10 +27,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-document.querySelector(".btn").addEventListener('click', promptMe);
+document.querySelector(".btn").addEventListener("click", promptMe);
 
 function promptMe() {
-    var buttonPrompt = prompt("Use special characters? y/n");
-    if (input === "y") {
-      selections = (specialCharacters)
+    var buttonPrompt = prompt(promptOne);
+    if () {
+      selections = (specialCharacters); console.log(selections)
 }}
